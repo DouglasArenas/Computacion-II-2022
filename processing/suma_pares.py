0737fcd86500f7fde2aed136534b3cea676873c7
DouglasArenas <douglasarenas71@gmail.com>
2022-03-21 18:24:30 -0300

    fork in process

--- processing/suma_pares.py
+++ processing/suma_pares.py
@@ -1,11 +1,18 @@
 from os import fork
 import time
 
+parser = argparse_ArgumentParser(description="Suma pares de pids entre 0 y pid del pid hijo")
 
+parser.add_argument('-n', "--number", type=int, help="Ingrese un numero")
+
+
+def sum():
+    pass
 
 
 
 if __name__ == "__main__":
     main()
+
 
 
