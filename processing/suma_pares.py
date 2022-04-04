4fdda76395e87500085150c80d9c097824505a1b
Douglas <douglasarenas71@gmail.com>
2022-04-03 23:50:28 -0300

    suma par del rango de procesos

--- processing/suma_pares.py
+++ processing/suma_pares.py
@@ -1,18 +1,22 @@
-from os import fork
-import time
+import os, argparse
 
-parser = argparse_ArgumentParser(description="Suma pares de pids entre 0 y pid del pid hijo")
-
-parser.add_argument('-n', "--number", type=int, help="Ingrese un numero")
-
-
-def sum():
-    pass
+parser = argparse.ArgumentParser(description="Suma pares de pids entre 0 y pid del proceso hijo")
+parser.add_argument('-n', type=int, help="Genera una cantidad de procesos según el número ingresado")
+parser.add_argument('-v', action='store_true',help="Activa el modo verboso")
+args = parser.parse_args()
 
 
+def child():
+    if os.fork() == 0:
+        suma = sum(n for n in range(os.getpid() + 1) if not n & 1)
+        if args.v:
+            print(f"Starting process {os.getpid()}")
+            print(f"Ending process {os.getpid()}")
+            print(f"{os.getpid()} - {os.getppid()} : {suma}")
+        else:
+            print(f"{os.getpid()} - {os.getppid()} : {suma}")
+        os._exit(0)
 
-if __name__ == "__main__":
-    main()
-
-
+for e in range(args.n):
+    child()
 
