c7b17bca91fb22a049f21b2032a99ddde30c7e4e
DouglasArenas <douglasarenas71@gmail.com>
2022-05-11 13:16:47 -0300

    clase 11/05

--- processing/inversor.py
+++ processing/inversor.py
@@ -2,12 +2,33 @@ import argparse, os
 
 parser = argparse.ArgumentParser(description="Archivo que genera procesos que invierten las lineas de un texto")
 parser.add_argument('-f', help="Ruta del archivo a trabajar")
-args = parser.parse_argument()
+args = parser.parse_args()
 
 file = open(args.f, 'r')
 r, w = os.pipe()
-r2, w2 = os.pipe()
+rr, ww = os.pipe()
+lines = file.readlines()
 
-for i in range(len(file.lines())):
+print(lines)
+line = "".join(lines)
+print(line)
+os.write(w, line.encode())
+
+for i in range(len(lines)):
     if os.fork() == 0:
-        w = os.fdopen(w)
+        os.close(w)
+        os.close(rr)
+        print("Child reading...")
+        r = os.fdopen(r)
+        text = r.read()
+        print(text)
+        print("Child writing...")
+        os.write(ww, text)
+        print("Child ending...")
+        os._exit(0)
+
+for i in range(len(lines)):
+    os.wait()
+print("father reading")
+rr = os.fdopen(rr)
+print("Father reading childs... ", rr.read())
